540e01e676bf43da60ddb2a599eadc7d782c5303
Vishal Rawat <vishalvr.2595@gmail.com>
2019-03-08 11:15:22 +0530

    Added changes in Button

--- assets/js/pleb.js
+++ assets/js/pleb.js
@@ -1,6 +1,8 @@
 function onSubmit(){
     let email = document.getElementById("email").value;
     let emailregx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let btnId =  document.getElementById("submit");
+    let btnDis = document.getElementById("upper");
     let isValid = false;
     if ( email !== "" ) {
         if(emailregx.test(email)){
@@ -11,6 +13,8 @@ function onSubmit(){
         alert("Enter a valid email!");
     }
     if(email) {
+        btnDis.style.backgroundColor = "#000000a6"
+        btnId.disabled = true;
         var data = {
             email: email,
         }
@@ -19,10 +23,11 @@ function onSubmit(){
         xmlHttp.open("POST", "https://us-central1-pleb-app.cloudfunctions.net/earlyAccess", true ); // false for synchronous request
         xmlHttp.setRequestHeader('Content-type','application/json; charset=utf-8');
         xmlHttp.onreadystatechange = function() {
-            console.log("asdasdasd", xmlHttp.readyState)
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
                 document.getElementById("email").value  = ""
                 alert("Success!")
+                btnId.disabled = false;
+                btnDis.style.backgroundColor = "#000000"
             }
         }
         xmlHttp.send(JSON.stringify({"email": email}))
